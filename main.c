6a9037441e8951ba6390359c8b2c5f7da317dda2
Srajan Rai <srajanope@gmail.com>
2022-06-21 13:34:45 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 
 int main(void) {
     clear();
-    printf("Hello World");
+    // printf("Hello World");
+
+    char a, b[10];
+    printf("Enter a char: ");
+    scanf("%c", &a);
+    fflush(stdin); // flush whole input cache
+    
+    printf("Enter a sentence: ");
+    gets(b);
+    printf("%c - %s", a, b);
+
+    
+
+
+
+
     return 0;
 }
