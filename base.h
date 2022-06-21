25a62488220c19caf5894112b7e5e305bd39d258
Srajan Rai <srajanope@gmail.com>
2022-06-21 20:13:16 +0530

    Update base.h

--- base.h
+++ base.h
@@ -1,5 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void clear(void) {
     system("cls");
 }
+
+int output =0;
+
+int octal2decimal(char * input) {
+    if(input[0] == '0') {
+        printf("This function is working\n");
+    }
+    else {
+        printf("Wrong Detection of Octa Number\n");
+        exit(1);
+    }
+    return output;
+}
+
+int octal2hexa(char * input) {
+    if(input[0] == '0') {
+        printf("This function is working\n");
+    }
+    else {
+        printf("Wrong Detection of Octa Number\n");
+        exit(1);
+    }
+    return output;
+}
+
+int decimal2octal(char * input) {
+    printf("This function is working\n");
+    return output;
+}
+
+int decimal2hexa(char * input) {
+    printf("This function is working\n");
+    return output;
+}
+
+int hexa2octal(char * input) {
+    if(input[0]== '0' && input[1] == 'x') {
+        printf("This function is working\n");
+    }
+    else {
+        printf("Wrong Detection of Octa Number\n");
+        exit(1);
+    }
+    return output;
+}
+
+int hexa2decimal(char * input) {
+    if(input[0]== '0' && input[1] == 'x') {
+        printf("This function is working\n");
+    }
+    else {
+        printf("Wrong Detection of Octa Number\n");
+        exit(1);
+    }
+    return output;
+}
+
